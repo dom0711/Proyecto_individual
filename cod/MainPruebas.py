0f33e9aa83e097d0e9af7a3621adbd588da5343d
Dominick <drt1415@gmail.com>
2024-06-08 19:40:25 -0600

    se prueban los nuevos métodos

--- cod/MainPruebas.py
+++ cod/MainPruebas.py
@@ -10,35 +10,102 @@ from Deck import Mazo
 
 import pandas as pd
 
-deck_stats = pd.read_excel("C:\\Users\\usuar\\Desktop\\CA0305\\Proyecto_individual\\docs\\deck_propio.xlsx")
-deck_list = ["Effect Veiler", "Effect Veiler", "Effect Veiler", "Rescue-ACE Hydrant", "Rescue-ACE Hydrant",
-             "Snake-Eyes Poplar", "Snake-Eyes Poplar", "Maxx C", "Maxx C", "Maxx C", 
-             "Ash Blossom & Joyous Springs", "Ash Blossom & Joyous Springs", "Ash Blossom & Joyous Springs",
-             "Rescue-ACE Impulse", "Rescue-ACE Impulse", "Rescue-ACE Air Lifter", "Rescue-ACE Air Lifter",
-             "Rescue-ACE Fire Attacker", "Rescue-ACE Fire Engine", "Diabellstar the Black Witch",
-             "Diabellstar the Black Witch", "Rescue-ACE Preventer", "Rescue-ACE Preventer", 
-             "Rescue-ACE Turbulunce", "Rescue-ACE Turbulunce", "Bonfire", "Bonfire", "Bonfire", 
-             "Original Sinful Spoils - Snake-Eye", "Original Sinful Spoils - Snake-Eye", "Rescue-ACE HQ",
-             "Called by the Grave", "Called by the Grave", "RESCUE!", "ALERT!", "EMERGENCY!", "EMERGENCY!",
-             "EMERGENCY!", "WANTED: Seeker of Sinful Spoils", "CONTAIN!", "EXTINGUISH!", 
-             "Sinful Spoils of Betrayal - Silvera"]
-deck_size = len(deck_list)
-num_starters = 12
-num_extenders = 5
-num_defensives = 13
-num_combo_pieces = 6
-num_garnets = 6
-num_non_engine = 0
+ruta_deck_propio = "C:\\Users\\usuar\\Desktop\\CA0305\\Proyecto_individual\\docs\\deck_propio.xlsx"
+deck_propio_stats = pd.read_excel(ruta_deck_propio)
+deck_propio_list = ["Effect Veiler", "Effect Veiler", "Effect Veiler","Jet Synchron", 
+                    "Kurikara Divincarnate", "Rescue-ACE Hydrant", "Rescue-ACE Hydrant","Snake-Eye Ash", 
+                    "Snake-Eye Oak", "Snake-Eyes Poplar", "Snake-Eyes Poplar", "Maxx C", "Maxx C", 
+                    "Maxx C", "Ash Blossom & Joyous Spring", "Ash Blossom & Joyous Spring", 
+                    "Ash Blossom & Joyous Spring", "Rescue-ACE Impulse", "Rescue-ACE Impulse", 
+                    "Rescue-ACE Air Lifter", "Rescue-ACE Air Lifter", "Rescue-ACE Air Lifter", 
+                    "Bystial Magnamhut", "Bystial Druiswurm", "Rescue-ACE Fire Attacker", 
+                    "Bystial Baldrake", "Kashtira Fenrir", "Kashtira Unicorn", "Rescue-ACE Fire Engine", 
+                    "Diabellstar the Black Witch", "Diabellstar the Black Witch", "Rescue-ACE Preventer",
+                    "Rescue-ACE Preventer", "Snake-Eyes Flamberge Dragon", "Snake-Eyes Flamberge Dragon",
+                    "Rescue-ACE Turbulence", "Rescue-ACE Turbulence", "Nibiru, the Primal Being", 
+                    "Reinforcement of the Army", "Sinful Spoils of Subversion - Snake-Eye", 
+                    "Bonfire", "Bonfire", "Bonfire", "Original Sinful Spoils - Snake-Eye", 
+                    "Original Sinful Spoils - Snake-Eye", "Rescue-ACE HQ", 
+                    "Pressured Plante Wraitsoth", "Divine Temple of the Snake-Eye", "Kashtira Birth", 
+                    "Called by the Grave", "Called by the Grave", "Crossout Designator", "RESCUE!", 
+                    "ALERT!", "EMERGENCY!", "EMERGENCY!", "EMERGENCY!", 
+                    "WANTED: Seeket of Sinful Spoils", "CONTAIN!", "EXTINGUISH!"]
 
-deck = Mazo(num_starters, num_extenders, num_defensives, num_combo_pieces, num_garnets, 
-            num_non_engine, deck_list, deck_stats)
+starters_propio = deck_propio_stats[deck_propio_stats["Utilidad"] == "Starter"]
+extenders_propio = deck_propio_stats[deck_propio_stats["Utilidad"] == "Extender"]
+defensives_propio = deck_propio_stats[deck_propio_stats["Utilidad"] == "Defensive"]
+combo_pieces_propio = deck_propio_stats[deck_propio_stats["Utilidad"] == "Combo piece"]
+garnets_propio = deck_propio_stats[deck_propio_stats["Utilidad"] == "Garnet"]
+non_engine_propio = deck_propio_stats[deck_propio_stats["Utilidad"] == "Non engine"]
 
-hand = deck.hand_sample(5)
-print(hand)
+
+num_starters_propio = starters_propio["Cantidad"].sum()
+num_extenders_propio = extenders_propio["Cantidad"].sum()
+num_defensives_propio = defensives_propio["Cantidad"].sum()
+num_combo_pieces_propio = combo_pieces_propio["Cantidad"].sum()
+num_garnets_propio = garnets_propio["Cantidad"].sum()
+num_non_engine_propio = non_engine_propio["Cantidad"].sum()
 
 
-deck.draw_card(deck.deck_list, hand)
-print(hand)
+
+deck_propio = Mazo(num_starters_propio, num_extenders_propio, num_defensives_propio, 
+                   num_combo_pieces_propio, num_garnets_propio, num_non_engine_propio, 
+                   deck_propio_list, deck_propio_stats)
+
+ruta_netdeck = "C:\\Users\\usuar\\Desktop\\CA0305\\Proyecto_individual\\docs\\netdecked.xlsx"
+netdeck_stats = pd.read_excel(ruta_netdeck)
+netdeck_list = ["Effect Veiler", "Effect Veiler", "Effect Veiler", "Rescue-ACE Hydrant", 
+                "Rescue-ACE Hydrant", "Snake-Eyes Poplar", "Snake-Eyes Poplar", "Maxx C", "Maxx C",
+                "Maxx C", "Ash Blossom & Joyous Spring", "Ash Blossom & Joyous Spring", 
+                "Ash Blossom & Joyous Spring", "Rescue-ACE Impulse", "Rescue-ACE Impulse", 
+                "Rescue-ACE Air Lifter", "Rescue-ACE Air Lifter", "Rescue-ACE Air Lifter", 
+                "Rescue-ACE Fire Engine", "Diabellstar the Black Witch", "Diabellstar the Black Witch", 
+                "Diabellstar the Black Witch", "Rescue-ACE Preventer", "Rescue-ACE Preventer",
+                "Rescue-ACE Turbulence", "Bonfire", "Bonfire", "Bonfire", 
+                "Original Sinful Spoils - Snake-Eye", "Original Sinful Spoils - Snake-Eye", 
+                "Called by the Grave", "Called by the Grave", "RESCUE!", "ALERT!", "EMERGENCY!", 
+                "EMERGENCY!", "EMERGENCY!", "WANTED: Seeket of Sinful Spoils", "CONTAIN!", 
+                "EXTINGUISH!"]
+
+starters_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Starter"]
+extenders_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Extender"]
+defensives_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Defensive"]
+combo_pieces_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Combo piece"]
+garnets_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Garnet"]
+non_engine_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Non engine"]
+
+
+num_starters_netdeck = starters_netdeck["Cantidad"].sum()
+num_extenders_netdeck = extenders_netdeck["Cantidad"].sum()
+num_defensives_netdeck = defensives_netdeck["Cantidad"].sum()
+num_combo_pieces_netdeck = combo_pieces_netdeck["Cantidad"].sum()
+num_garnets_netdeck = garnets_netdeck["Cantidad"].sum()
+num_non_engine_netdeck = non_engine_netdeck["Cantidad"].sum()
+
+
+
+netdeck = Mazo(num_starters_netdeck, num_extenders_netdeck, num_defensives_netdeck, 
+                   num_combo_pieces_netdeck, num_garnets_netdeck, num_non_engine_netdeck, 
+                   netdeck_list, netdeck_stats)
+
+
+# hand_propio = deck_propio.hand_sample(5)
+# hand_netdeck = netdeck.hand_sample(5)
+
+# print("Mano propia: \n" f'{hand_propio}')
+# print("Mano netdeck: \n" f'{hand_netdeck}')
+
+# hand_propio_samples = []
+# hand_netdeck_samples = []
+
+# tourney_hands = deck_propio.tourney_sample()
+
+# for i in range(0, 4):
+#     print("Mano " f'{i + 1}' ": " f'{tourney_hands[i]}' "\n") 
+
+
+
+
 
 
 
