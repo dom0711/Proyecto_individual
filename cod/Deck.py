a05307d8dc2a9e2531244c3d664dd98b2d70572a
Dominick <drt1415@gmail.com>
2024-06-11 19:55:32 -0600

    feat: se completan los métodos calc_hypergeom y rank_hand

--- cod/Deck.py
+++ cod/Deck.py
@@ -159,7 +159,7 @@ class Mazo():
         '''
         mano = []
         import random
-        import pandas as pd
+        import pandas
         for i in range(0, 5):
             draw = self.__deck_list[random.randint(0, len(self.__deck_list) - 1)]
             mano.append(draw)
@@ -243,32 +243,102 @@ class Mazo():
                           # lo complementan.
         # Vamos a guardar en una lista la utilidad de cada una de las cartas de la mano
         util_hand = []
-        for i in range(0, len(hand) - 1):
+        # Además, quiero guardar la cantidad de cada categoría que contiene la mano, entonces inicializo
+        # en cero cada una y les voy agregando conforme revisa la mano.
+        starters = 0
+        extenders = 0
+        defensives = 0
+        combo_pieces = 0
+        garnets = 0
+        non_engine = 0
+        for i in range(0, len(hand)):
             draw = hand[i]
             indice_draw = self.__card_stats.index[self.__card_stats["Carta"] == draw][0]
             if self.__card_stats.loc[indice_draw, "Utilidad"] == "Starter":
                 util_hand.append("Starter")
+                starters = starters + 1
             elif self.__card_stats.loc[indice_draw, "Utilidad"] == "Extender":
                 util_hand.append("Extender")
+                extenders = extenders + 1
             elif self.__card_stats.loc[indice_draw, "Utilidad"] == "Defensive":
                 util_hand.append("Defensive")
+                defensives = defensives + 1
             elif self.__card_stats.loc[indice_draw, "Utilidad"] == "Combo piece":
                 util_hand.append("Combo piece")
+                combo_pieces = combo_pieces + 1
             elif self.__card_stats.loc[indice_draw, "Utilidad"] == "Garnet":
-                util_hand("Garnet")
+                util_hand.append("Garnet")
+                garnets = garnets + 1
             elif self.__card_stats.loc[indice_draw, "Utilidad"] == "Non engine":
-                util_hand.append("Non engine") 
+                util_hand.append("Non engine")
+                non_engine = non_engine + 1
+        # Ahora tenemos una lista que incluye la utilidad de cada carta en la mano, tenemos que
+        # determinar que tan buena es la mano.
+        # Considerando las reglas del juego y los principios y valores que les dimos a cada categoría
+        # podemos darnos una idea de cual sería la mejor mano posible, para esto definimos:
+            # 1. A pesar de ser las cartas más importantes, tener más de una starter en la mano no es
+            #    lo mejor, pues usualmente solo es posible usar un starter por turno
+            # 2. Las cartas defensivas aportan mucho pues aseguran de que el combo se complete.
+            # 3. En caso de que el starter sea interrumpido o que alguna parte del combo no se logre,
+            #    un extender permite continuar el combo.
+            # 4. El non-engine aporta muy poco al combo directamente, más que todo permite hacerlo un 
+            #    poco más fuerte, entonces no es necesario tener estas cartas en la mano.
+        # Con estas consideraciones podemos definir algunos rangos o categorias:
+            # S+: Una mano que tiene 1 starter, 1 extender, 2 defensive, 1 non_engine
+            # S: Una mano que tiene 1 starter, 1 extender, 1 defensive, y las otras dos cartas pueden
+            #    ser cualesquiera, excepto garnets.
+            # A: Una mano que tiene 1 starter, 0 extender, 1 defensive, y las otras tres pueden ser
+            #    cualesquiera, excepto garnets.
+            # B: Una mano que tiene 1 starter, 1 extender, 0 defensive, y las otras tres pueden ser
+            #    cualesquiera, excepto garnets.
+            # C: Una mano que tiene 1 starter, 0 extender, 0 defensive y las otras tres pueden ser
+            #    cualesquiera, incluidas garnets.
+            # D: Toda mano que no contenga un starter.
+            # F: Toda mano que no contenga un starter y uno o más garnets.
+        if garnets == 0:
+            if starters == 0:
+                return "D"
+            else:
+                if defensives == 0:
+                    if extenders == 0:
+                        return "C"
+                    else:
+                        return "B"
+                else:
+                    if extenders == 0:
+                        return "A"
+                    else:
+                        return "S"
+        else:
+            if starters == 0:
+                return "F"
             
+        # if starters == 0:
+        #     if garnets >= 2:
+        #         return "F"
+        #     else:
+        #         return "D"
+        # else:
+        #     if (extenders == 0) and (defensives == 0):
+        #         return "C"
+        #     else:
+        # elif (extenders == 1) and (defensives == 2) and (non_engine == 1):
+        #     return "S"
+        # elif (non_engine == 0) and (garnets == 0):
+        #     if (extenders >= 1) and (defensives >= 1) and ()
+        #     return "B"
+        # elif 
         
-    def calc_hypergeom(self, uti_ideal, cant_ideal, cant_draw):
+    def calc_hypergeom(self, util_ideal, cant_deseo, cant_draw):
         '''
         Método que determina la probabilidad de tomar cant_ideal de cartas de una utilidad 
         especifica en una cantidad establecida de draws.
         
         Parametros:
-            uti_ideal: la utilidad de la carta que se desea tomar del deck, tipo string.
+            util_ideal: la utilidad de la carta que se desea tomar del deck, tipo string.
             cant_draw: la cantidad de cartas que toma del deck para intentar sacar la carta, tipo int
-            cant_ideal: la cantidad de cartas de la utilidad uti_ideal que desea tomar del deck, tipo int
+            cant_deseo: la cantidad de cartas de la utilidad uti_ideal que desea tomar del deck, 
+                        tipo int
             
         Returns:
             prob_exito: la probabilidad de que saque una carta de la utilidad deseada, tipo float
@@ -276,15 +346,27 @@ class Mazo():
         # La probabilidad de tomar una carta especifica del deck en una cantidad determinada de draws
         # se comporta como una distribución hipergeométrica, pues se toman cartas del deck y no se 
         # devuelven, entonces puedo calcular la probabilidad.
-        # Considere P(X = cant_ideal) = [(K, k) * (N - K, n-k)] / [(N, n)] donde los parentesis son
+        # Considere P(X = j) = [(J, j) * (N - J, n - j)] / [(N, n)] donde los parentesis son
         # coeficientes binomiales y:
             # N: cantidad de cartas en el deck
-            # K: cantidad de cartas con la utilidad deseada en el deck
+            # J: cantidad de cartas con la utilidad deseada en el deck
             # n: cantidad de cartas que se toman del deck
-            # k: cantidad de cartas de la utilidad deseada que se quieren sacar
-        
-        
-        
+            # j: cantidad de cartas de la utilidad deseada que se quieren sacar
+        import math
+        util_lower = util_ideal.lower()
+        # Para obtener el atributo usando el parametro util_ideal y sin hacer un montón de ifs vamos a
+        # usar el comando getattr(), se toma como base lo visto en el siguiente sitio web:
+            # https://micro.recursospython.com/recursos/la-funcion-getattr.html
+        # Además, voy a usar nombres cortos y sencillos para las variables porque la formula es larga.
+        J = getattr(self, util_lower)
+        j = cant_deseo
+        N = len(self.__deck_list)
+        n = cant_draw
+        bino_1 = (math.factorial(J)) / ((math.factorial(j)) * (math.factorial(J - j)))
+        bino_2 = (math.factorial(N - J)) / ((math.factorial(n - j)) * (math.factorial(N - J - n + j)))
+        bino_3 = (math.factorial(N)) / ((math.factorial(n)) * (math.factorial(N - n)))
+        prob_exito = (bino_1 * bino_2) / bino_3
+        return prob_exito 
         
         
         
