c11efebb0d99368bac47d90eaa8d4c2890ec6943
Dominick <drt1415@gmail.com>
2024-06-14 16:05:32 -0600

    se prueba el método reset_deck()

--- cod/MainPruebas.py
+++ cod/MainPruebas.py
@@ -12,6 +12,7 @@ import pandas as pd
 
 ruta_deck_propio = "C:\\Users\\usuar\\Desktop\\CA0305\\Proyecto_individual\\data\\deck_propio.xlsx"
 deck_propio_stats = pd.read_excel(ruta_deck_propio)
+
 deck_propio_list = ["Effect Veiler", "Effect Veiler", "Effect Veiler","Jet Synchron", 
                     "Kurikara Divincarnate", "Rescue-ACE Hydrant", "Rescue-ACE Hydrant","Snake-Eye Ash", 
                     "Snake-Eye Oak", "Snake-Eyes Poplar", "Snake-Eyes Poplar", "Maxx C", "Maxx C", 
@@ -46,47 +47,49 @@ num_combo_pieces_propio = combo_pieces_propio["Cantidad"].sum()
 num_garnets_propio = garnets_propio["Cantidad"].sum()
 num_non_engine_propio = non_engine_propio["Cantidad"].sum()
 
-
+deck_propio_inicial = [num_starters_propio, num_extenders_propio,
+                       num_defensives_propio, num_combo_pieces_propio, num_garnets_propio,
+                       num_non_engine_propio, deck_propio_list, deck_propio_stats]
 
 deck_propio = Mazo(num_starters_propio, num_extenders_propio, num_defensives_propio, 
                    num_combo_pieces_propio, num_garnets_propio, num_non_engine_propio, 
-                   deck_propio_list, deck_propio_stats)
+                   deck_propio_list, deck_propio_stats, deck_propio_inicial)
 
-ruta_netdeck = "C:\\Users\\usuar\\Desktop\\CA0305\\Proyecto_individual\\data\\netdecked.xlsx"
-netdeck_stats = pd.read_excel(ruta_netdeck)
-netdeck_list = ["Effect Veiler", "Effect Veiler", "Effect Veiler", "Rescue-ACE Hydrant", 
-                "Rescue-ACE Hydrant", "Snake-Eyes Poplar", "Snake-Eyes Poplar", "Maxx C", "Maxx C",
-                "Maxx C", "Ash Blossom & Joyous Spring", "Ash Blossom & Joyous Spring", 
-                "Ash Blossom & Joyous Spring", "Rescue-ACE Impulse", "Rescue-ACE Impulse", 
-                "Rescue-ACE Air Lifter", "Rescue-ACE Air Lifter", "Rescue-ACE Air Lifter", 
-                "Rescue-ACE Fire Engine", "Diabellstar the Black Witch", "Diabellstar the Black Witch", 
-                "Diabellstar the Black Witch", "Rescue-ACE Preventer", "Rescue-ACE Preventer",
-                "Rescue-ACE Turbulence", "Bonfire", "Bonfire", "Bonfire", 
-                "Original Sinful Spoils - Snake-Eye", "Original Sinful Spoils - Snake-Eye", 
-                "Called by the Grave", "Called by the Grave", "RESCUE!", "ALERT!", "EMERGENCY!", 
-                "EMERGENCY!", "EMERGENCY!", "WANTED: Seeket of Sinful Spoils", "CONTAIN!", 
-                "EXTINGUISH!"]
+# ruta_netdeck = "C:\\Users\\usuar\\Desktop\\CA0305\\Proyecto_individual\\data\\netdecked.xlsx"
+# netdeck_stats = pd.read_excel(ruta_netdeck)
+# netdeck_list = ["Effect Veiler", "Effect Veiler", "Effect Veiler", "Rescue-ACE Hydrant", 
+#                 "Rescue-ACE Hydrant", "Snake-Eyes Poplar", "Snake-Eyes Poplar", "Maxx C", "Maxx C",
+#                 "Maxx C", "Ash Blossom & Joyous Spring", "Ash Blossom & Joyous Spring", 
+#                 "Ash Blossom & Joyous Spring", "Rescue-ACE Impulse", "Rescue-ACE Impulse", 
+#                 "Rescue-ACE Air Lifter", "Rescue-ACE Air Lifter", "Rescue-ACE Air Lifter", 
+#                 "Rescue-ACE Fire Engine", "Diabellstar the Black Witch", "Diabellstar the Black Witch", 
+#                 "Diabellstar the Black Witch", "Rescue-ACE Preventer", "Rescue-ACE Preventer",
+#                 "Rescue-ACE Turbulence", "Bonfire", "Bonfire", "Bonfire", 
+#                 "Original Sinful Spoils - Snake-Eye", "Original Sinful Spoils - Snake-Eye", 
+#                 "Called by the Grave", "Called by the Grave", "RESCUE!", "ALERT!", "EMERGENCY!", 
+#                 "EMERGENCY!", "EMERGENCY!", "WANTED: Seeket of Sinful Spoils", "CONTAIN!", 
+#                 "EXTINGUISH!"]
 
-starters_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Starter"]
-extenders_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Extender"]
-defensives_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Defensive"]
-combo_pieces_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Combo piece"]
-garnets_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Garnet"]
-non_engine_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Non engine"]
-
-
-num_starters_netdeck = starters_netdeck["Cantidad"].sum()
-num_extenders_netdeck = extenders_netdeck["Cantidad"].sum()
-num_defensives_netdeck = defensives_netdeck["Cantidad"].sum()
-num_combo_pieces_netdeck = combo_pieces_netdeck["Cantidad"].sum()
-num_garnets_netdeck = garnets_netdeck["Cantidad"].sum()
-num_non_engine_netdeck = non_engine_netdeck["Cantidad"].sum()
+# starters_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Starter"]
+# extenders_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Extender"]
+# defensives_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Defensive"]
+# combo_pieces_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Combo piece"]
+# garnets_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Garnet"]
+# non_engine_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Non engine"]
 
 
+# num_starters_netdeck = starters_netdeck["Cantidad"].sum()
+# num_extenders_netdeck = extenders_netdeck["Cantidad"].sum()
+# num_defensives_netdeck = defensives_netdeck["Cantidad"].sum()
+# num_combo_pieces_netdeck = combo_pieces_netdeck["Cantidad"].sum()
+# num_garnets_netdeck = garnets_netdeck["Cantidad"].sum()
+# num_non_engine_netdeck = non_engine_netdeck["Cantidad"].sum()
 
-netdeck = Mazo(num_starters_netdeck, num_extenders_netdeck, num_defensives_netdeck, 
-                   num_combo_pieces_netdeck, num_garnets_netdeck, num_non_engine_netdeck, 
-                   netdeck_list, netdeck_stats)
+
+
+# netdeck = Mazo(num_starters_netdeck, num_extenders_netdeck, num_defensives_netdeck, 
+#                    num_combo_pieces_netdeck, num_garnets_netdeck, num_non_engine_netdeck, 
+#                    netdeck_list, netdeck_stats)
 
 
 hand_propio = deck_propio.hand_sample(5)
@@ -103,11 +106,9 @@ hand_propio = deck_propio.hand_sample(5)
 # for i in range(0, 4):
 #     print("Mano " f'{i + 1}' ": " f'{tourney_hands[i]}' "\n")
 
-score = deck_propio.rank_hand(hand_propio)
+#score = deck_propio.rank_hand(hand_propio)
 
 
-
-
-
+deck_propio.reset_deck()
 
 
