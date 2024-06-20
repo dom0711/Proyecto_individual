7d65ce4bc862ace2cc8644277d906bd668036c20
Dominick <drt1415@gmail.com>
2024-06-20 03:23:25 -0600

    hago pruebas de algunas funciones del interfaz para confirmar informacion

--- cod/MainPruebas.py
+++ cod/MainPruebas.py
@@ -43,41 +43,9 @@ def deck_creator(ruta_deck):
 
 deck_propio = deck_creator(ruta_deck_propio)
 
-# ruta_netdeck = "C:\\Users\\usuar\\Desktop\\CA0305\\Proyecto_individual\\data\\netdecked.xlsx"
-# netdeck_stats = pd.read_excel(ruta_netdeck)
-# netdeck_list = ["Effect Veiler", "Effect Veiler", "Effect Veiler", "Rescue-ACE Hydrant", 
-#                 "Rescue-ACE Hydrant", "Snake-Eyes Poplar", "Snake-Eyes Poplar", "Maxx C", "Maxx C",
-#                 "Maxx C", "Ash Blossom & Joyous Spring", "Ash Blossom & Joyous Spring", 
-#                 "Ash Blossom & Joyous Spring", "Rescue-ACE Impulse", "Rescue-ACE Impulse", 
-#                 "Rescue-ACE Air Lifter", "Rescue-ACE Air Lifter", "Rescue-ACE Air Lifter", 
-#                 "Rescue-ACE Fire Engine", "Diabellstar the Black Witch", "Diabellstar the Black Witch", 
-#                 "Diabellstar the Black Witch", "Rescue-ACE Preventer", "Rescue-ACE Preventer",
-#                 "Rescue-ACE Turbulence", "Bonfire", "Bonfire", "Bonfire", 
-#                 "Original Sinful Spoils - Snake-Eye", "Original Sinful Spoils - Snake-Eye", 
-#                 "Called by the Grave", "Called by the Grave", "RESCUE!", "ALERT!", "EMERGENCY!", 
-#                 "EMERGENCY!", "EMERGENCY!", "WANTED: Seeket of Sinful Spoils", "CONTAIN!", 
-#                 "EXTINGUISH!"]
+ruta_netdeck = "C:\\Users\\usuar\\Desktop\\CA0305\\Proyecto_individual\\data\\netdecked.xlsx"
 
-# starters_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Starter"]
-# extenders_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Extender"]
-# defensives_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Defensive"]
-# combo_pieces_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Combo piece"]
-# garnets_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Garnet"]
-# non_engine_netdeck = netdeck_stats[netdeck_stats["Utilidad"] == "Non engine"]
-
-
-# num_starters_netdeck = starters_netdeck["Cantidad"].sum()
-# num_extenders_netdeck = extenders_netdeck["Cantidad"].sum()
-# num_defensives_netdeck = defensives_netdeck["Cantidad"].sum()
-# num_combo_pieces_netdeck = combo_pieces_netdeck["Cantidad"].sum()
-# num_garnets_netdeck = garnets_netdeck["Cantidad"].sum()
-# num_non_engine_netdeck = non_engine_netdeck["Cantidad"].sum()
-
-
-
-# netdeck = Mazo(num_starters_netdeck, num_extenders_netdeck, num_defensives_netdeck, 
-#                    num_combo_pieces_netdeck, num_garnets_netdeck, num_non_engine_netdeck, 
-#                    netdeck_list, netdeck_stats)
+netdeck = deck_creator(ruta_netdeck)
 
 
 # hand_propio = deck_propio.hand_sample(5)
@@ -89,7 +57,7 @@ deck_propio = deck_creator(ruta_deck_propio)
 # hand_propio_samples = []
 # hand_netdeck_samples = []
 
-tourney_hands = deck_propio.tourney_sample()
+# tourney_hands = deck_propio.tourney_sample()
 
 # for i in range(0, 4):
 #     print("Mano " f'{i + 1}' ": " f'{tourney_hands[i]}' "\n")
@@ -100,8 +68,8 @@ tourney_hands = deck_propio.tourney_sample()
 
 # deck_propio.reset_deck()
 
-total_scores = []
-for i in range(0, 25):
-    total_scores.append(deck_propio.eval_deck())
+# total_scores = []
+# for i in range(0, 25):
+#     total_scores.append(deck_propio.eval_deck())
     
-print(total_scores)
+# print(total_scores)
